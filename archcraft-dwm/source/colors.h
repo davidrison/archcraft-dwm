884b5da5abeda202d01c85ddf31ea09c38fb83f1
David W. Rison <david.rison@liferay.com>
2022-05-05 12:27:06 -0400

    Rounded corners, inactive dimmed, colors debugging

--- archcraft-dwm/source/colors.h
+++ archcraft-dwm/source/colors.h
@@ -2,7 +2,9 @@ static const char black[]       = "#1e222a";
 static const char gray2[]       = "#2e323a"; // unfocused window border
 static const char gray3[]       = "#545862";
 static const char gray4[]       = "#6d8dad";
-static const char blue[]        = "#61AFEF"; // focused window border
+//static const char blue[]        = "#0020d5"; // focused window border
+//static const char blue[]        = "#61AFEF"; // focused window border
+static const char blue[]        = "#3f5bd5"; // focused window border
 static const char green[]       = "#98C379";
 static const char red[]         = "#E06C75";
 static const char orange[]      = "#E49263";
@@ -11,3 +13,11 @@ static const char pink[]        = "#E98CA4";
 static const char purple[]      = "#C678DD";
 static const char cyan[]        = "#56B6C2";
 static const char col_borderbar[]  = "#1e222a"; // inner border
+static const char drac_fg[] = "#BFBFBF";
+static const char drac_bg[] = "#282A36";
+static const char drac_border[] = "#282a36";
+static const char drac_sel_fg[] = "#F8F8F2";
+static const char drac_sel_bg[] = "#6272A4";
+static const char drac_sel_border[] = "#6272A4";
+
+
--- archcraft-dwm/source/config.def.h
+++ archcraft-dwm/source/config.def.h
@@ -7,13 +7,13 @@
  * */
 
 /* ******************** Configurations ******************** */
-static const unsigned int borderpx  		= 1;	/* border pixel of windows */
+static const unsigned int borderpx  		= 0;	/* border pixel of windows */
 static const unsigned int default_border 	= 0;	// to switch back to default border after dynamic border resizing via keybinds
 static const unsigned int snap      		= 32;	/* snap pixel */
-static const unsigned int gappih    		= 5;	/* horiz inner gap between windows */
-static const unsigned int gappiv    		= 5;	/* vert inner gap between windows */
-static const unsigned int gappoh    		= 5;	/* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    		= 5;	/* vert outer gap between windows and screen edge */
+static const unsigned int gappih    		= 15;	/* horiz inner gap between windows */
+static const unsigned int gappiv    		= 15;	/* vert inner gap between windows */
+static const unsigned int gappoh    		= 15;	/* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    		= 15;	/* vert outer gap between windows and screen edge */
 static 				  int smartgaps         = 0;	/* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning 	= 0;	/* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing 	= 10;	/* systray spacing */
@@ -42,7 +42,9 @@ static const int colorfultag        		= 1;  /* 0 means use SchemeSel for selecte
 static const char *colors[][3]  = {
     /*               	   fg      bg      border   */
     [SchemeNorm]       = { gray3,  black,  gray2 },
+    //[SchemeNorm]       = { drac_fg,  drac_bg,  drac_border },
     [SchemeSel]        = { gray4,  blue,   blue  },
+    //[SchemeSel]        = { drac_sel_fg,  drac_sel_bg,   drac_sel_border  },
     [TabSel]           = { blue,   gray2,  black  },
     [TabNorm]          = { gray3,  black,  black },
     [SchemeTag]        = { gray3,  black,  black },
