0f5f3c3902e9001abe468ae6035efbfb166bf5a4
David Rison <david.rison@liferay.com>
2022-05-11 12:34:44 -0400

    Changes to opacity

--- archcraft-dwm/shared/bin/screenlayout.sh
+++ archcraft-dwm/shared/bin/screenlayout.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 2560x1080 --pos 0x0 --rotate normal
+#xrandr --output DisplayPort-2 --dpi 96 
+xrandr --output DisplayPort-2 --rate 119.98 --dpi 96 --primary --mode 1920x1080 --pos 1680x0 --rotate normal --output HDMI-A-0 --mode 1680x1050 --pos 0x0 --rotate normal
+#xrandr --output DisplayPort-0 --off --output DisplayPort-1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DisplayPort-2 --off --output HDMI-A-0 --mode 2560x1080 --pos 0x0 --rotate normal
